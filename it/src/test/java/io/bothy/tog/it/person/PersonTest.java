1d8e30bda9dd08b16731edc6aeecd9ebce3dcb7e
Neil Green <neil@bothy.technology>
2024-11-17 00:37:45 +0000

    Add some integration tests.

---
+++ it/src/test/java/io/bothy/tog/it/person/PersonTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2024 Neil Richard Green
+ *
+ * This file is part of Tog.
+ *
+ * Tog is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Tog is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Foobar. If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+package io.bothy.tog.it.person;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class PersonTest {
+
+    @Test
+    void canBuildPerson() {
+        final var person = Person.builder()
+                .withName("John Doe")
+                .withAge(62)
+                .withAdult(true)
+                .withAddress("Toulouse Street")
+                .build();
+
+        assertThat(person).extracting("name").isEqualTo("John Doe");
+        assertThat(person).extracting("age").isEqualTo(62);
+        assertThat(person).extracting("adult").isEqualTo(true);
+        assertThat(person).extracting("address").isEqualTo("Toulouse Street");
+    }
+}
