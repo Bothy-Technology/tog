a840fc63f61edece93fd52ad4e38e4d2697ad49a
Neil Green <neil@bothy.technology>
2024-11-17 08:42:34 +0000

    Rename `it` to `examples`.

--- examples/src/main/java/io/bothy/tog/examples/sb/Market.java
+++ examples/src/main/java/io/bothy/tog/examples/sb/Market.java
@@ -12,13 +12,14 @@
  * You should have received a copy of the GNU General Public License along with Foobar. If not, see
  * <https://www.gnu.org/licenses/>.
  */
-package io.bothy.tog.it.person;
+package io.bothy.tog.examples.sb;
 
 import io.bothy.tog.Builder;
+import java.util.List;
 
 @Builder
-public record Person(String name, int age, boolean adult, String address) {
-    public static PersonBuilder.WithName builder() {
-        return PersonBuilder.builder();
+public record Market(String name, List<Selection> selections) {
+    public static MarketBuilder.WithName builder() {
+        return MarketBuilder.builder();
     }
 }
--- examples/src/test/java/io/bothy/tog/examples/sb/EventTest.java
+++ examples/src/test/java/io/bothy/tog/examples/sb/EventTest.java
@@ -12,7 +12,7 @@
  * You should have received a copy of the GNU General Public License along with Foobar. If not, see
  * <https://www.gnu.org/licenses/>.
  */
-package io.bothy.tog.it.sb;
+package io.bothy.tog.examples.sb;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
