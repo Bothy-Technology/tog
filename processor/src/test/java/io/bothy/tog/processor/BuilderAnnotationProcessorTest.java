3858ad9c04774dd4a60bf16c9759dda3abd779cd
Neil Green <neil@bothy.technology>
2024-11-24 13:14:06 +0000

    Move `BuilderAnnotationProcessor` and related classes to `processor` package.

--- processor/src/test/java/io/bothy/tog/processor/BuilderAnnotationProcessorTest.java
+++ processor/src/test/java/io/bothy/tog/processor/BuilderAnnotationProcessorTest.java
@@ -12,7 +12,7 @@
  * You should have received a copy of the GNU General Public License along with Foobar. If not, see
  * <https://www.gnu.org/licenses/>.
  */
-package io.bothy.tog;
+package io.bothy.tog.processor;
 
 import static com.google.testing.compile.CompilationSubject.assertThat;
 import static com.google.testing.compile.Compiler.javac;
